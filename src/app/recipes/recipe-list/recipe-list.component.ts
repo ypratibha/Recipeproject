1d5f9895efcc3ae61c3bd9ef56625114aedc3f0d
ypratibha <yadagiri.pratibha@gmail.com>
2022-08-11 17:15:20 +0530

    Initial commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'New Test Recipe',
+      'This is simply a test recipe',
+      'https://www.washingtonpost.com/resizer/WZA859eqdl1br735VlartjQpvYo=/arc-anglerfish-washpost-prod-washpost/public/KO6XMOQNAEI63CHIYWG4HW5O4I.jpg'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
